83eb35cfebf0fe6527f069a669b2a4334664f8c2
448 OG <superuser@448.africa>
2026-07-16 16:27:21 +0300

    Add ability to read browser clipboard

--- src/window_ops/clipboard.rs
+++ src/window_ops/clipboard.rs
@@ -12,4 +12,17 @@ impl WasmWindow {
 
         Ok(())
     }
+
+    pub async fn read_clipboard(&self) -> WasmToolkitResult<String> {
+        let pending: JsFuture = self.navigator().clipboard().read_text().into();
+
+        let outcome = pending.await.map_err(|error| {
+            WasmToolkitError::parse_js_error(error, "Unable to read the contents in the clipboard")
+        })?;
+
+        outcome.as_string().ok_or(WasmToolkitError::JsError {
+            name: "Not a String".to_string(),
+            message: outcome.js_typeof().as_string().unwrap_or_default(),
+        })
+    }
 }
